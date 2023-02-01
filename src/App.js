f204a300e277c9187b8099dad16f82e9f61b8244
John Quarmor <johnequarmor@gmail.com>
2023-02-01 12:46:45 +0000

    Imported & rendered HelloWorld module.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import HelloWorld from "./components/HelloWorld";
 
 const App = () => {
 	return (
 		<div>
-			<h1>Hello World!</h1>
+			<HelloWorld />
 		</div>
 	);
 };
