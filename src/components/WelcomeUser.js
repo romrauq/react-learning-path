29fabdc5f8758f890427d213d8500b80c39e1fc9
John Quarmor <johnequarmor@gmail.com>
2023-02-01 12:51:40 +0000

    Created stateless function component that takes prop.

---
+++ src/components/WelcomeUser.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// A stateless function componentwhich takes the prop "user":
+const WelcomeUser = ({ user }) => {
+	return <p>Welcome {user}</p>;
+};
+
+export default WelcomeUser;
