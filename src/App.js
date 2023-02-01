93bb74f6b28057c67a6b34ce1bc14cfaddc14c67
John Quarmor <johnequarmor@gmail.com>
2023-02-01 12:52:23 +0000

    Imported, rendered & passed prop to WelcomeUser.js component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import HelloWorld from "./components/HelloWorld";
+import WelcomeUser from "./components/WelcomeUser";
 
 const App = () => {
 	return (
 		<div>
 			<HelloWorld />
+			<WelcomeUser user={"John Doe"} />
 		</div>
 	);
 };
