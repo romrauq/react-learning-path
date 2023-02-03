d18c63704c0285e42807096745019b83b929d092
John Quarmor <johnequarmor@gmail.com>
2023-02-03 12:41:52 +0000

    Created class component

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class ClassComponent extends Component {
+	state = {};
+	render() {
+		return <p>Class Component from Component</p>;
+	}
+}
+
+export default ClassComponent;
---
+++ src/components/ClassReactComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class ClassReactComponent extends React.Component {
+	state = {};
+	render() {
+		return <p>Class Component from React.Component</p>;
+	}
+}
+
+export default ClassReactComponent;
