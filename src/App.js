87223af99565abb218d350d1ae44d9f1e7faac76
John Quarmor <johnequarmor@gmail.com>
2023-02-03 12:42:09 +0000

    Imported & rendered class component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 import WelcomeUser from "./components/WelcomeUser";
+import ClassComponent from "./components/ClassComponent";
+import ClassReactComponent from "./components/ClassReactComponent";
 
 const App = () => {
 	return (
 		<div>
 			<HelloWorld />
 			<WelcomeUser user={"John Doe"} />
+			<ClassComponent />
+			<ClassReactComponent />
 		</div>
 	);
 };
