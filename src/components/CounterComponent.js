88b98b0996408a5e94451009a6d8beaeac5b5237
John Quarmor <johnequarmor@gmail.com>
2023-02-03 22:29:10 +0000

    Created a functional component handling and changing state values using the useState hook.

---
+++ src/components/CounterComponent.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const CounterComponent = () => {
+	const [count, setCount] = useState(0);
+
+	const increment = () => {
+		setCount(count + 1);
+	};
+
+	const decrement = () => {
+		setCount(count - 1);
+	};
+
+	return (
+		<div>
+			<p>Our number is: {count}</p>
+			<button onClick={increment}>Increase Number</button>
+			<button onClick={decrement}>Decrease Number</button>
+		</div>
+	);
+};
+
+export default CounterComponent;
