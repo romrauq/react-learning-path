2a6ae6e0f42bf7266b411871a56fae2c9d33a8cc
John Quarmor <johnequarmor@gmail.com>
2023-02-03 13:03:51 +0000

    Corrected props parameter script of function.

--- src/components/WelcomeUser.js
+++ src/components/WelcomeUser.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 // A stateless function componentwhich takes the prop "user":
-const WelcomeUser = ({ user }) => {
-	return <p>Welcome {user}</p>;
+const WelcomeUser = (props) => {
+	return <p>Welcome {props.user}</p>;
 };
 
 export default WelcomeUser;
