c6545bb804a6f0a09a015dd64152e71ce93ba005
John Quarmor <johnequarmor@gmail.com>
2023-02-03 22:18:56 +0000

    Created a functional component handling and changing state values using the useState hook.

---
+++ src/components/UseStateChange.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const UseStateChange = () => {
+	const [username, setUsername] = useState("Johann Liebert");
+	const [inputValue, setInputValue] = useState("");
+
+	const handleChange = (e) => {
+		// console.log("Input changed!");
+		setInputValue(e.target.value);
+	};
+
+	const handleClick = () => {
+		// console.log("Button clicked");
+		setUsername(inputValue);
+	};
+
+	return (
+		<div>
+			<p>My name is {username}</p>
+			<input
+				type="text"
+				name=""
+				id=""
+				value={inputValue}
+				placeholder="Enter your new name value here"
+				onChange={handleChange}
+			/>
+			<button onClick={handleClick}>Edit Name</button>
+		</div>
+	);
+};
+
+export default UseStateChange;
