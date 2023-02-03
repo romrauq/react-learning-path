a8524221e53e26d8d5b24b1bfe4a47cd7498d342
John Quarmor <johnequarmor@gmail.com>
2023-02-03 17:29:18 +0000

    Rendered ChangeState component.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
-import WelcomeUser from "./components/WelcomeUser";
-import ClassComponent from "./components/ClassComponent";
-import ClassReactComponent from "./components/ClassReactComponent";
+// import HelloWorld from "./components/HelloWorld";
+// import WelcomeUser from "./components/WelcomeUser";
+// import ClassComponent from "./components/ClassComponent";
+// import ClassReactComponent from "./components/ClassReactComponent";
+// import UsingState from "./components/UsingState";
+import ChangeState from "./components/ChangeState";
 
 const App = () => {
 	return (
 		<div>
-			<HelloWorld />
-			<WelcomeUser user={"John Doe"} />
-			<ClassComponent />
-			<ClassReactComponent />
+			<ChangeState />
 		</div>
 	);
 };
