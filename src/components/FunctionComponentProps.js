cd51530d701805facc16614d7ee3b70ba7729f9b
John Quarmor <johnequarmor@gmail.com>
2023-02-04 07:23:22 +0000

    Created function component which takes a prop.

---
+++ src/components/FunctionComponentProps.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// A stateless function componentwhich takes the prop "user":
+const FunctionComponentProps = (props) => {
+	return <p>Welcome {props.user}</p>;
+};
+
+export default FunctionComponentProps;
