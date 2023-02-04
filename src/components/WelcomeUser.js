7ea611203b9a2c1510e23d215506686da30b9f64
John Quarmor <johnequarmor@gmail.com>
2023-02-04 07:22:41 +0000

    Deleted unused file

--- src/components/WelcomeUser.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// A stateless function componentwhich takes the prop "user":
-const WelcomeUser = (props) => {
-	return <p>Welcome {props.user}</p>;
-};
-
-export default WelcomeUser;
