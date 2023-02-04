8abaa0879d50e6d732f91a13a98b914ed5c09b7a
John Quarmor <johnequarmor@gmail.com>
2023-02-04 07:24:08 +0000

    Imported component which passes props.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
-import WelcomeUser from "./components/WelcomeUser";
 import ClassComponent from "./components/ClassComponent";
 import ClassReactComponent from "./components/ClassReactComponent";
+import ClassComponentProps from "./components/ClassComponentProps";
 import ClassState from "./components/ClassState";
 import ChangeClassState from "./components/ChangeClassState";
 import FunctionComponent from "./components/FunctionComponent";
+import FunctionComponentProps from "./components/FunctionComponentProps";
 import FunctionState from "./components/FunctionState";
 import FunctionStateChange from "./components/FunctionStateChange";
 import FunctionStateChangeTwo from "./components/FunctionStateChangeTwo";
@@ -13,12 +14,13 @@ import FunctionStateChangeTwo from "./components/FunctionStateChangeTwo";
 const App = () => {
 	return (
 		<div>
-			<WelcomeUser />
 			<ClassComponent />
 			<ClassReactComponent />
+			<ClassComponentProps user="Nanabi-san" />
 			<ClassState />
 			<ChangeClassState />
 			<FunctionComponent />
+			<FunctionComponentProps user={"Shikamaru"} />
 			<FunctionState />
 			<FunctionStateChange />
 			<FunctionStateChangeTwo />
