85d967751a6faefe0ceb89ee3423322655f25b47
John Quarmor <johnequarmor@gmail.com>
2023-02-05 10:57:29 +0000

    Removed unsued setted function from useState hook definition.

--- src/components/FunctionState.js
+++ src/components/FunctionState.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const FunctionState = () => {
-	const [username, setUsername] = useState("John Doeux");
+	const [username] = useState("John Doeux");
 	return <p>My name is {username}</p>;
 };
 
