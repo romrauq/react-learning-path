6ea7658a91f9f2745475a265830ec6dc5431e8f9
John Quarmor <johnequarmor@gmail.com>
2023-02-05 10:58:05 +0000

    Renamed file module.

--- src/components/FunctionStateChangeTwo.js
+++ src/components/FunctionStateChangeTwo.js
@@ -1,33 +1,23 @@
 import React, { useState } from "react";
 
-const FunctionStateChangeTwo = () => {
-	const [username, setUsername] = useState("Johann Liebert");
-	const [inputValue, setInputValue] = useState("");
+const FunctionStateChange = () => {
+	const [count, setCount] = useState(0);
 
-	const handleChange = (e) => {
-		// console.log("Input changed!");
-		setInputValue(e.target.value);
+	const increment = () => {
+		setCount(count + 1);
 	};
 
-	const handleClick = () => {
-		// console.log("Button clicked");
-		setUsername(inputValue);
+	const decrement = () => {
+		setCount(count - 1);
 	};
 
 	return (
 		<div>
-			<p>My name is {username}</p>
-			<input
-				type="text"
-				name=""
-				id=""
-				value={inputValue}
-				placeholder="Enter your new name value here"
-				onChange={handleChange}
-			/>
-			<button onClick={handleClick}>Edit Name</button>
+			<p>Our number is: {count}</p>
+			<button onClick={increment}>Increase Number</button>
+			<button onClick={decrement}>Decrease Number</button>
 		</div>
 	);
 };
 
-export default FunctionStateChangeTwo;
+export default FunctionStateChange;
