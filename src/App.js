d6efc2ee64e2ef33792ef65fa3860b1e22b695f6
John Quarmor <johnequarmor@gmail.com>
2023-02-05 21:02:22 +0000

    Imported and comented out component modules

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React from "react";
 import "./App.css";
-import ClassComponent from "./components/ClassComponent";
-import ClassReactComponent from "./components/ClassReactComponent";
-import ClassComponentProps from "./components/ClassComponentProps";
-import ClassState from "./components/ClassState";
-import ChangeClassState from "./components/ChangeClassState";
-import FunctionComponent from "./components/FunctionComponent";
-import FunctionComponentProps from "./components/FunctionComponentProps";
-import FunctionState from "./components/FunctionState";
-import FunctionStateChange from "./components/FunctionStateChange";
-import FunctionStateChangeTwo from "./components/FunctionStateChangeTwo";
+// import ClassComponent from "./components/ClassComponent";
+// import ClassReactComponent from "./components/ClassReactComponent";
+// import ClassComponentProps from "./components/ClassComponentProps";
+// import ClassState from "./components/ClassState";
+// import ChangeClassState from "./components/ChangeClassState";
+// import FunctionComponent from "./components/FunctionComponent";
+// import FunctionComponentProps from "./components/FunctionComponentProps";
+// import ReactEvents from "./components/ReactEvents";
+// import ReactEventsArgs from "./components/ReactEventsArgs";
+// import FunctionState from "./components/FunctionState";
+// import FunctionStateChangeOne from "./components/FunctionStateChangeOne";
+// import FunctionStateChangeTwo from "./components/FunctionStateChangeTwo";
+// import ConditionalRendering from "./components/ConditionalRendering";
+// import ConditionalRenderingTwo from "./components/ConditionalRenderingTwo";
 
 const App = () => {
-	return (
-		<div>
-			<ClassComponent />
-			<ClassReactComponent />
-			<ClassComponentProps user="Nanabi-san" />
-			<ClassState />
-			<ChangeClassState />
-			<FunctionComponent />
-			<FunctionComponentProps user={"Shikamaru"} />
-			<FunctionState />
-			<FunctionStateChange />
-			<FunctionStateChangeTwo />
-		</div>
-	);
+	return <div></div>;
 };
 
 export default App;
