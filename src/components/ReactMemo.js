fc548b93f31f55179e2a1735a0310c83a5a4e1f1
John Quarmor <johnequarmor@gmail.com>
2023-02-07 21:09:07 +0000

    Created component to showcase event render issue.

---
+++ src/components/ReactMemo.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+// Any time the increment button is clicked, the Todos components is re-rendered.
+// This could cause performance issues if the component was more complex.
+const ToDos = ({ todos }) => {
+	console.log(`The "Todos" component was rendered!`);
+	return (
+		<div>
+			<h1>My Todos</h1>
+			{todos.map((todo, index) => {
+				return <p key={index}>{todo}</p>;
+			})}
+		</div>
+	);
+};
+
+const ReactMemo = () => {
+	let [count, setCount] = useState(0);
+	let [todos, setTodos] = useState(["todo item 1", "todo item 2"]);
+
+	const increment = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<div>
+			<ToDos todos={todos} />
+			<hr />
+			<div>
+				Count: {count}
+				<button onClick={increment}>Increase Count</button>
+			</div>
+		</div>
+	);
+};
+
+export default ReactMemo;
