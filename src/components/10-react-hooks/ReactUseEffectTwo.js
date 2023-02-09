5647eff434b00aea910f962f30cc64f77fdb751f
John Quarmor <johnequarmor@gmail.com>
2023-02-09 17:54:01 +0000

    Created component that its useEfect hook is dependent on the variable "count" .

---
+++ src/components/10-react-hooks/ReactUseEffectTwo.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+const ReactUseEffectTwo = () => {
+	const [count, setCount] = useState(0);
+	const [formula, setFormula] = useState(0);
+
+	useEffect(() => {
+		setFormula(() => count * 5);
+	}, [count]); // This useEffect hook is dependent on the ${count} variable.
+	// If the ${count} variable updates, the effect will run again.
+
+	return (
+		<div>
+			<p>Count: {count}</p>
+			<button onClick={() => setCount((c) => c + 1)}> Addition</button>
+			<p>Calculation: {formula}</p>
+		</div>
+	);
+};
+
+export default ReactUseEffectTwo;
