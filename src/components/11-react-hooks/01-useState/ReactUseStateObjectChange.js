7707a81951531e226cd238b4c4900c352f7888bb
John Quarmor <johnequarmor@gmail.com>
2023-02-14 08:30:15 +0000

    Added new button

--- src/components/11-react-hooks/01-useState/ReactUseStateObjectChange.js
+++ src/components/11-react-hooks/01-useState/ReactUseStateObjectChange.js
@@ -4,12 +4,24 @@ const ReactUseStateObjectChange = () => {
 	const [car, updateCar] = useState({
 		name: "Toyota",
 		model: "Camry",
-		year: "2010",
+		year: 2010,
 	});
+
+	const updateName = () => {
+		updateCar((prevState) => {
+			return { ...prevState, name: "Mercedes Benz" };
+		});
+	};
 
 	const updateModel = () => {
 		updateCar((prevState) => {
-			return { ...prevState, model: "Navara" };
+			return { ...prevState, model: "M Series" };
+		});
+	};
+
+	const updateYear = () => {
+		updateCar((prevState) => {
+			return { ...prevState, year: 2023 };
 		});
 	};
 
@@ -18,7 +30,9 @@ const ReactUseStateObjectChange = () => {
 			<p>
 				I drive a {car.year} {car.name} {car.model}
 			</p>
+			<button onClick={updateName}>Update Name</button>
 			<button onClick={updateModel}>Update Model</button>
+			<button onClick={updateYear}>Update Year</button>
 		</div>
 	);
 };
