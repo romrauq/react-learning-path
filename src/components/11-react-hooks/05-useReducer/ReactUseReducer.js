7c75feaf0c838c641a1f400f875b5bafefa47ca5
John Quarmor <johnequarmor@gmail.com>
2023-02-18 17:23:01 +0000

    Added comments.

--- src/components/11-react-hooks/05-useReducer/ReactUseReducer.js
+++ src/components/11-react-hooks/05-useReducer/ReactUseReducer.js
@@ -1,10 +1,12 @@
 import React, { useReducer } from "react";
 
+// Define a global constant object variable:
 const ACTIONS = {
 	DECREMENT: "decrement",
 	INCREMENT: "increment",
 };
 
+// Create function that can be used to determine the state change based on passed action argument:
 const reducer = (state, action) => {
 	switch (action.type) {
 		case ACTIONS.DECREMENT:
@@ -17,14 +19,15 @@ const reducer = (state, action) => {
 };
 
 const ReactUseReducer = () => {
+	// Define the component state using the useReducer() hook.
 	const [state, dispatch] = useReducer(reducer, { count: 0 });
 
 	const decrement = () => {
-		dispatch({ type: ACTIONS.DECREMENT });
+		dispatch({ type: ACTIONS.DECREMENT }); // Set the action type action to be passed to the reducer function.
 	};
 
 	const increment = () => {
-		dispatch({ type: ACTIONS.INCREMENT });
+		dispatch({ type: ACTIONS.INCREMENT }); // Set the action type action to be passed to the reducer function.
 	};
 
 	return (
