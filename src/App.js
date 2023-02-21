6fb83ba1d9ee006dba7993664ef2b72b980df2ee
John Quarmor <johnequarmor@gmail.com>
2023-02-21 07:52:46 +0000

    Removed commented out import paths.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,7 @@
 import React from "react";
 import "./App.css";
-// import ClassComponent from "./components/01-react-components/ClassComponent";
-// import ClassReactComponent from "./components/01-react-components/ClassReactComponent";
-// import FunctionComponent from "./components/01-react-components/FunctionComponent";
-// import ClassComponentProps from "./components/02-react-props/ClassComponentProps";
-// import FunctionComponentProps from "./components/02-react-props/FunctionComponentProps";
-// import ReactEvents from "./components/03-react-events/ReactEvents";
-// import ReactEventsArgs from "./components/03-react-events/ReactEventsArgs";
-// import ClassState from "./components/04-component-state/ClassState";
-// import ChangeClassState from "./components/04-component-state/ChangeClassState";
-// import FunctionState from "./components/04-component-state/FunctionState";
-// import FunctionStateChangeOne from "./components/04-component-state/FunctionStateChangeOne";
-// import FunctionStateChangeTwo from "./components/04-component-state/FunctionStateChangeTwo";
-// import ConditionalRendering from "./components/05-conditional-rendering/ConditionalRendering";
-// import ConditionalRenderingTwo from "./components/05-conditional-rendering/ConditionalRenderingTwo";
-// import ReactLists from "./components/06-react-lists/ReactLists";
-// import ReactListsKeys from "./components/06-react-lists/ReactListsKeys";
-// import ReactForm from "./components/07-react-forms/ReactForm";
-// import SubmitForm from "./components/07-react-forms/SubmitForm";
-// import ReactFormMultipleFields from "./components/07-react-forms/ReactFormMultipleFields";
-// import ReactFormTextArea from "./components/07-react-forms/ReactFormTextArea";
-// import ReactSelectForm from "./components/07-react-forms/ReactSelectForm";
-// import ReactMemo from "./components/08-react-memo/ReactMemo";
-// import ReactMemoFix from "./components/08-react-memo/ReactMemoFix";
-// import ReactStylingInline from "./components/10-css-styling/ReactStylingInline";
-// import ReactStylingObject from "./components/10-css-styling/ReactStylingObject";
-// import ReactUseState from "./components/11-react-hooks/01-useState/ReactUseState";
-// import ReactUseStateObject from "./components/11-react-hooks/01-useState/ReactUseStateObject";
-// import ReactUseStateObjectChange from "./components/11-react-hooks/01-useState/ReactUseStateObjectChange";
-// import ReactPrevState from "./components/11-react-hooks/01-useState/ReactPrevState";
-// import ReactUseEffect from "./components/11-react-hooks/02-useEffect/ReactUseEffect";
-// import ReactUseEffectTwo from "./components/11-react-hooks/02-useEffect/ReactUseEffectTwo";
-// import ReactUseEffectCleanup from "./components/11-react-hooks/02-useEffect/ReactUseEffectCleanup";
-// import ParentComponent from "./components/11-react-hooks/03-useContext/ParentComponent";
-// import ReactUseRef from "./components/11-react-hooks/04-useRef/ReactUseRef";
-// import AccessingDomElements from "./components/11-react-hooks/04-useRef/AccessingDomElements";
-// import ReactUseReducer from "./components/11-react-hooks/05-useReducer/ReactUseReducer";
-// import ReactUseReducerRevision from "./components/11-react-hooks/05-useReducer/ReactUseReducerRevision";
-// import ReactUseCallback from "./components/11-react-hooks/06-useCallback/ReactUseCallback";
-import ReactUseMemo from "./components/11-react-hooks/07-useMemo/ReactUseMemo";
+
+import ReactUseMemo from "./components/09-react-hooks/07-useMemo/ReactUseMemo";
 
 const App = () => {
 	return (
